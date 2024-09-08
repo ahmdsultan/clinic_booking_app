95fc1aaa6e1de9c50df8fc425f8679a53c4067d8
Abobakr <202110101763@student.ust.edu.ye>
2024-09-08 23:31:39 +0300

    ازلنا علامة وضع الديبج

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SigninScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SigninScreen());
   }
 }
