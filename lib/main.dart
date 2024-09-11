33c675cc76ec00eb8a7d3f4249b859074712c7df
Abobakr <202110101763@student.ust.edu.ye>
2024-09-11 22:02:55 +0300

    تهيئة الفايربيس لتفادي اخطاء التهيئة عند التشغيل للبرنامج
    في صفحة تسجيل الدخول شرحنا طريقة استخدام
    كلاس الازرار الجديد
    
    في صفحة بوتون ويدجيت اضفنا خاصية اونبريسد لعمل اكشن معين عند الضغد على
    الزر

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
+    WidgetsFlutterBinding.ensureInitialized(); // اضافة سطر تهيئة الفاير بيس لتفادي خطأ تهيئة الفايربيس عند عمل تشغيل للبرنامج
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
